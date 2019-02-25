488d7ed3d9845e8adeccccd5f9abdc9f3c38a4f4
jackstapleton <47336976+jackstapleton@users.noreply.github.com>
2019-02-25 21:07:28 +0000

    tested version of bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
+PASS=$1
+
+echo 'Running bootstrap.sh'
+
 # yum installs
 sudo yum update -y 
 sudo yum install gcc -y 
 
 # anaconda installs
-wget https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh
-bash Anaconda3-2018.12-Linux-x86_64.sh
-rm Anaconda3-2018.12-Linux-x86_64.sh
+wget https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh -O ~/conda.sh
+bash ~/conda.sh -b -p ~/anaconda3
+rm ~/conda.sh
+export PATH=$PATH:~/anaconda3/bin
+echo 'export PATH=$PATH:"~/anaconda3/bin"' >> ~/.bash_profile
+source ~/.bash_profile
 
 # kdb installs
-conda install -c kx kdb
-conda install -c kx embedpy
-conda install -c kx jupyterq
+conda install -cy kx kdb
+conda install -cy kx embedpy
+conda install -cy kx jupyterq
 pip install qpython
 
 # git install
 sudo yum install git
-git clone https://jackstapleton:$1@github.com/jackstapleton/advanced-kdb-cmtp.git
-git clone https://jackstapleton:$1@github.com/jackstapleton/vim-qkdb-syntax-gruvbox.git
-git clone https://jackstapleton:$1@github.com/jackstapleton/dot-files.git
+git clone https://jackstapleton:$PASS@github.com/jackstapleton/advanced-kdb-cmtp.git ~/repos/advanced-kdb-cmtp.git
+git clone https://jackstapleton:$PASS@github.com/jackstapleton/vim-qkdb-syntax-gruvbox.git ~/repos/im-qkdb-syntax-gruvbox.git
+git clone https://jackstapleton:$PASS@github.com/jackstapleton/dot-files.git ~/repos/dot-files
 
 # vim installs
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s dot-files/vimrc .vimrc
-vim +PluginInstall +qall
+ln -s ~/repos/dot-files/vimrc ~/.vimrc
+
+echo 'bootstrap.sh completed successfully'
