9f425bb7fdb58b2c68fa1d563632ab60fa372567
jackstapleton <jack.stapleton@outlook.com>
2019-12-24 13:31:08 +0000

    remove sudo user

--- aws/ubuntu-dev-ami/install-scripts/create-ami.sh
+++ aws/ubuntu-dev-ami/install-scripts/create-ami.sh
@@ -7,4 +7,4 @@ AZ=$(ec2metadata --availability-zone)
 AMINAME=${AZ::-1}-dev-ec2.ami-$AMIDATE
 
 INSTANCEID=$(ec2metadata --instance-id)
-sudo -i -u $USER aws ec2 create-image --instance-id $INSTANCEID --name $AMINAME
+aws ec2 create-image --instance-id $INSTANCEID --name $AMINAME
