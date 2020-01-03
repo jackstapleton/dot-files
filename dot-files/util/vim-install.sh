4bae26eea63b01198395586e7773f81076bcbc34
jackstapleton <jack.stapleton@outlook.com>
2020-01-03 17:34:18 +0000

    dont install ycm

--- dot-files/util/vim-install.sh
+++ dot-files/util/vim-install.sh
@@ -19,21 +19,21 @@ vim +PluginInstall +qall
 
 ln -s $DOTFILESDIR/vim.plugin $HOME/.vim/plugin
 
-# Python completion engine only configured currently for ubuntu
-LINUX_DISTRIBUTION=`cat /etc/*release | grep ^NAME= | cut -d '=' -f 2`
-echo $LINUX_DISTRIBUTION
+# # Python completion engine only configured currently for ubuntu
+# LINUX_DISTRIBUTION=`cat /etc/*release | grep ^NAME= | cut -d '=' -f 2`
+# echo $LINUX_DISTRIBUTION
 
-if [ $LINUX_DISTRIBUTION = '"Ubuntu"' ]; then
-    if [ -d $HOME/.vim/bundle/YouCompleteMe ]; then
-        read -p "Do you want to install Valloric/YouCompleteMe ? (y/n): " INSTALL_YCM
-        if [ $INSTALL_YCM = "y" ]; then
-            echo "Installing YCM"
-            sudo apt install git build-essential cmake python3-dev -y
-            cd $HOME/.vim/bundle/YouCompleteMe
-            python3 install.py
-            cd $CURRENTDIR
-        fi
-    fi
-fi
+# if [ $LINUX_DISTRIBUTION = '"Ubuntu"' ]; then
+#     if [ -d $HOME/.vim/bundle/YouCompleteMe ]; then
+#         read -p "Do you want to install Valloric/YouCompleteMe ? (y/n): " INSTALL_YCM
+#         if [ $INSTALL_YCM = "y" ]; then
+#             echo "Installing YCM"
+#             sudo apt install git build-essential cmake python3-dev -y
+#             cd $HOME/.vim/bundle/YouCompleteMe
+#             python3 install.py
+#             cd $CURRENTDIR
+#         fi
+#     fi
+# fi
 
 echo "$0 Complete"
